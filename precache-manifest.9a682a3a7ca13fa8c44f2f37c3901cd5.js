cc9b0e7b8599fa64dd7c0fb50612a6c3d8c164ca
raytheboffin <raytheboffin@gmail.com>
2022-09-27 14:34:23 +0300

    Updates

---
+++ precache-manifest.9a682a3a7ca13fa8c44f2f37c3901cd5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "5452ded71ec5d9188e1c",
+    "url": "/random-quote-generator/static/css/main.a806229c.chunk.css"
+  },
+  {
+    "revision": "5452ded71ec5d9188e1c",
+    "url": "/random-quote-generator/static/js/main.5452ded7.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/random-quote-generator/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "c45cb79cf760195f9e24",
+    "url": "/random-quote-generator/static/js/2.c45cb79c.chunk.js"
+  },
+  {
+    "revision": "10376fe5ee5bb1b79389",
+    "url": "/random-quote-generator/static/js/runtime~main.10376fe5.js"
+  },
+  {
+    "revision": "2e04a38f70201373065de4e445cb1b9a",
+    "url": "/random-quote-generator/static/media/background3.2e04a38f.jpeg"
+  },
+  {
+    "revision": "9e2fa5c22c0a9303c62d19dc38ae5af4",
+    "url": "/random-quote-generator/static/media/twitter.9e2fa5c2.jpg"
+  },
+  {
+    "revision": "a936fe9580c9e7cda2a9be55d3278e6b",
+    "url": "/random-quote-generator/index.html"
+  }
+];
